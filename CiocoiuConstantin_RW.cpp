fc7723a44911e0917ad6eaf461443b88db3135cb
CiocoiuConstantin <35453799+CiocoiuConstantin@users.noreply.github.com>
2019-04-03 09:29:50 +0300

    Add files via upload

---
+++ CiocoiuConstantin_RW.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int n;
+int j;
+int k;
+int i;
+fstream f("input.dat",ios::in);
+fstream g("output.dat",ios::out);
+
+for(int i=1;i<=n;i++)
+{
+        for (int j=1;j<=n;j++)
+    {
+        for(int k=1;k<=n;k++)
+        {
+            if(k!=i&k!=j&i!=j)
+        }
+    }
+}
+
+
+int main()
+{
+
+}
+
+
